013153ee55c0d883986d83d5b3c1861e21b5237c
Mudasar Yaqoob <mudasaryaqoob29@gmail.com>
2024-06-25 16:46:40 +0500

    first commit

--- app.js
+++ app.js
@@ -92,3 +92,24 @@ var swiper = new Swiper(".mySwipers", {
       },
     },
   });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const contactUsHeading = document.querySelector('.about-projects-heading');
+    const contactUsButton = document.querySelector('.about-button');
+    
+    const observerOptions = {
+    threshold: 0.1 
+    };
+    
+    const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+            observer.unobserve(entry.target);
+        }
+    });
+    }, observerOptions);
+    
+    observer.observe(contactUsHeading);
+    observer.observe(contactUsButton);
+    });
